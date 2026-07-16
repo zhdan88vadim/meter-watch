4a047876d6d25406e892911efb696168baa4d741
zhdan88vadim@gmail.com <zhdan88vadim@gmail.com>
2026-07-16 14:06:55 +0300

    remove crop preview component

--- web/meter-watch/src/app/app.routes.ts
+++ web/meter-watch/src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
 
-// app.routes.ts
 export const routes: Routes = [
   { path: '', redirectTo: 'recognition', pathMatch: 'full' },
-  {
-    path: 'crop-editor',
-    loadComponent: () =>
-      import('./features/crop-input/components/crop-editor/crop-editor.component').then((m) => m.CropEditorComponent),
-  },
   {
     path: 'recognition',
     loadChildren: () =>
@@ -15,8 +9,4 @@ export const routes: Routes = [
         (m) => m.DIGIT_RECOGNITION_ROUTES,
       ),
   },
-  //   {
-  //     path: 'users',
-  //     loadChildren: () => import('./features/users/users.routes').then((m) => m.USER_ROUTES),
-  //   },
 ];
